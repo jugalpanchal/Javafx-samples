5500054e5c99cb3bc4ee888dee823bd4d5bb2ef0
Jugal Panchal <jugalpanchal@gmail.com>
2017-05-29 04:55:27 +0530

    Show main view

--- javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/automobileapp/Shell.java
+++ javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/automobileapp/Shell.java
@@ -1,13 +1,26 @@
 package com.jugalpanchal.javafxprototype.automobileapp;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Shell extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("com/jugalpanchal/javafxprototype/views/AutoMobileAppView.fxml"));
+			AnchorPane root = (AnchorPane) fxmlLoader.getRoot();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
