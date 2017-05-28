70059df9032e952292df7263657e933831d53e79
Jugal Panchal <jugalpanchal@gmail.com>
2017-05-29 05:22:30 +0530

    Added controls

--- javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/automobileapp/Shell.java
+++ javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/automobileapp/Shell.java
@@ -13,7 +13,7 @@ public class Shell extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/jugalpanchal/javafxprototype/views/AutoMobileAppView.fxml"));
-			AnchorPane root = (AnchorPane) fxmlLoader.load();
+			VBox root = (VBox) fxmlLoader.load();
 			Scene scene = new Scene(root,400,400);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
