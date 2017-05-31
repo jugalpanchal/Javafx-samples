bb9c2defcb6bac94d3ec0de83dd2f3df516abf18
Jugal Panchal <jugalpanchal@gmail.com>
2017-05-31 08:15:46 +0530

    Added some variant into table

--- javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/models/VariantModel.java
+++ javafxprototype/src/main/java/com/jugalpanchal/javafxprototype/models/VariantModel.java
@@ -1,5 +1,15 @@
 package com.jugalpanchal.javafxprototype.models;
 
 public class VariantModel extends BaseModel {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
